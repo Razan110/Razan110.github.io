a30de268941f5cdad2e759a59e3ddb381315decb
hoom <hoom@MacBook-Pro-alkhas-b-Mohammed.local>
2023-11-06 14:00:05 +0300

    inital commit

---
+++ projects/aster/HomePage.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Aster Beauty</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://localhost:8888/login/css/bootstrap.min.css" />
+    <link href="HomePage.css" type="text/css"rel="stylesheet">
+    <script src="Javascript.js" defer></script>
+</head>
+<body>
+    <nav>
+    <div class="Aster">
+        <img src="image/Aster.JPG">
+        <div class="bar">
+            <a href="#Home">Home</a>
+            <a href="AboutUs.html">About Us</a>
+            <a href="products.php">Products</a>
+            <a href="ContactUs.html">Contact Us</a>
+            <a href="#">Profile</a>
+           </div>
+  
+        </div>
+        <div class="logIn">
+            <a href="SignIn.html">Sign In</a>
+            <a href="Signup.html">Sign up</a>
+          </div>
+    </nav>
+
+    <div class="AsterImg">
+    <input type="text" placeholder="Search for a product!" name="Keyword">
+    
+    <button type="button"><img src="image/magnifying-glass copy.png" style="width: 20px; height: 20px;"></button>
+    </div>
+<section class="ouerServices">
+    <div class="whatIsAster">
+        <h1>About Aster</h1>
+        <p>A leader in prestige omni-retail, our mission at Aster is to create a welcoming beauty shopping experience for all and inspire fearlessness in our community.<br>
+We operate over 2,700 stores in 35 countries worldwide, with an expanding base of over 500 stores across the Americas, and a world-class ecommerce site.Since opening our first US store in New York’s SoHo neighborhood in 1998, Aster has been an industry-leading champion of diversity, inclusivity, and empowerment in the US, guided by our longstanding company values.
+Owned by LVMH Moët Hennessy Louis Vuitton, the world's leading luxury goods group, Aster was founded in France by Dominique Mandonnaud in 1970 and is defined by its unique, open-sell environment with an ever-increasing assortment of products from carefully curated brands, featuring emerging favorites, trusted classics, and Aster’s own line, Aster Collection.<br> Aster continues to curate innovative brands including a robust Clean beauty assortment and exclusive brands like Rare Beauty by Selena Gomez and FENTY BEAUTY by Rihanna brought to market first by Aster. Today, Aster is a powerful beauty presence in countries around the world thanks to our unparalleled assortment of prestige products in every category, unbiased service from beauty experts, interactive shopping environment, and relentless innovation, 
+including our expanding array of the ways in which clients can shop with Aster.</p>
+
+<h2>Commitment to Diversity and Inclusion</h2>
+<p>At Aster, our diversity and inclusion mission is simple: To never stop championing all beauty fearlessly and building inclusive environments for our employees, consumers, and communities. In 2019, Aster announced a new tagline and manifesto, “We Belong to Something Beautiful,” to reinforce our dedication to fostering belonging among all clients and employees and to publicly strive for a more inclusive vision for retail in the Americas. In 2020 Aster launched our Diversity and Inclusion Heart Journey to support our vision of becoming the Diversity, Inclusion, and Equity Champion in the retail industry.</p>
+    </div>
+
+</section>
+<h3>Products</h3>
+
+<table>
+    <th>
+<div class="newInProducts" >
+<section class="newIn">
+
+<div class="slideshow-container">
+<div class="mySlides fade">
+  <img src="newInProducts/p1.jpeg" >
+  </div>
+<div class="mySlides fade">
+  <img src="newInProducts/p2.jpg">
+</div>
+<div class="mySlides fade">
+  <img src="newInProducts/p3.jpg" >
+</div>
+<div class="ShopeNow">
+<h4>In With The New MakeUp!</h4>
+<p>The lastest beauty form the hotest brand.</p>
+<a href="products.php">Shope Now!</a>
+</div>
+</div>
+</section>
+</th>
+<th>
+<div class="skincare" >
+<section class="skinCarenewIn">
+
+<div class="slideshow-container-skincare">
+<div class="mySlides-skincare fade-skincare">
+  <img src="newInProducts/SkinCare/p1.jpg" >
+  </div>
+<div class="mySlides-skincare fade-skincare">
+  <img src="newInProducts/SkinCare/p2.jpg">
+</div>
+<div class="mySlides-skincare fade-skincare">
+  <img src="newInProducts/SkinCare/p3.jpg" >
+</div>
+<div class="mySlides-skincare fade-skincare">
+  <img src="newInProducts/SkinCare/p4.jpg" >
+</div>
+<div class="mySlides-skincare fade-skincare">
+  <img src="newInProducts/SkinCare/p5.jpg" >
+</div>
+<div class="skinCareShopeNow">
+<h4>In With The New SkinCare!</h4>
+<p>The lastest SkinCare form the hotest brand.</p>
+<a href="products.php">Shope Now!</a>
+</div>
+</div>
+</div>
+</section>
+</th>
+<th>
+
+<div class="Haircare" >
+<section class="HaircarenewIn">
+
+<div class="slideshow-container-Haircare">
+<div class="mySlides-Haircare fade-Haircare">
+  <img src="newInProducts/HairCare/p1.jpg" >
+  </div>
+
+<div class="mySlides-Haircare fade-Haircare">
+  <img src="newInProducts/HairCare/p3.jpg" >
+</div>
+<div class="HairCareShopeNow">
+<h4>In With The New HairCare!</h4>
+<p>The lastest SkinCare form the hotest brand.</p>
+<a href="products.php">Shope Now!</a>
+</div>
+</div>
+</div>
+</section>
+</th>
+</table>
+</div>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/OKSJ9NPw76g?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+<footer class="footer-distributed">
+	<div class="footer-left">
+		<p class="footer-links">
+			<a class="link-1" href="#">Home</a>
+
+			<a href="AboutUs.html">About Us</a>
+
+			<a href="ContactUs.html">Contact</a>
+			</p>
+			<p>Company Name &copy; 2015</p>
+	</div>
+</footer>
+
+</body>
+
+</html>
+
---
+++ projects/aster/products.php
@@ -0,0 +1,572 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://localhost:8888/login/css/bootstrap.min.css" />
+    <link href="products.css" type="text/css"rel="stylesheet">
+    <script src="Javascript.js" defer></script>
+</head>
+
+    <body>
+    <nav>
+    <div class="Aster">
+        <img src="image/Aster.JPG">
+        <div class="bar">
+            <a href="HomePage.php">Home</a>
+            <a href="AboutUs.html">About Us</a>
+            <a href="products.php">Products</a>
+            <a href="ContactUs.html">Contact Us</a>
+            <a href="#">Profile</a>
+            
+        </div>
+        <div class="logIn">
+                <a href="SignIn.html">Sign In</a>
+                <a href="Signup.html">Sign up</a>
+              </div>
+    </nav>
+
+    <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">Products Categorise</button>
+            <div id="myDropdown" class="dropdown-content">
+             <a href="#MakeUp">MakeUp</a>
+             <a href="#SkinCare">SkinCare</a>
+            <a href="#HairCare">HairCare</a>
+            <a href="#Perfume">Perfume</a>
+            <a href="#Bath & Body">Bath & Body</a>
+            </div>
+            </div>
+
+
+            <div class="shopping-cart">
+ 
+
+
+
+            <div class="displaybox">
+  <h1 id="MakeUp">Makeup</h1>
+  <div class="roll">
+  <div class="dis dis1">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup1"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis dis2">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup2"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis dis3">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup3"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis dis4">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis dis5">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis dis6">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>MakeUp</strong> Beauty Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoMakeup()' id="Makup"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+</div>
+ </div>
+
+<div class="shopping-cart2">
+ 
+
+ <div class="displaybox2">
+<h1 id="SkinCare">SkinCare</h1>
+  <div class="roll">
+  <div class="dis2 dis7">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>SkinCare</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis2 dis8">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>SkinCare</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare1"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis2 dis9">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>SkinCare</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare2"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis2 dis10">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>SkinCare</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare3"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis2 dis11">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>SkinCare</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare4"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis2 dis12">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Skin Care</strong> SkinCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoSkinCare()' id="SkinCare5"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+ </div>
+</div>
+           
+
+
+<div class="shopping-cart3">
+ 
+
+ <div class="displaybox3">
+<h1 id="HairCare">HairCare</h1>
+  <div class="roll">
+  <div class="dis3 dis13">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare1"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis3 dis14">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare2"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis3 dis15">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare3"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis3 dis16">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare4"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis3 dis17">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare5"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis3 dis18">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>HairCare</strong> HairCare Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoHairCare()' id="HairCare6"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+ </div>
+</div>
+
+<div class="shopping-cart4">
+ 
+
+ <div class="displaybox4">
+<h1 id="Perfume">Perfume</h1>
+  <div class="roll">
+  <div class="dis4 dis19">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume1"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis4 dis20">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume2"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis4 dis21">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume3"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis4 dis22">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume4"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis4 dis23">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume5"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis4 dis24">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Perfume</strong> Perfume Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoPerfum()' id="Perfume6"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+ </div>
+</div>
+
+<div class="shopping-cart5">
+ 
+
+ <div class="displaybox5">
+<h1 id="Bath & Body">Bath & Body</h1>
+  <div class="roll">
+  <div class="dis5 dis25">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body1"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis5 dis26">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body2"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis5 dis27">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body3"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis5 dis28">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body4"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis5 dis29">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body5"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+  
+  <div class="roll">
+  <div class="dis5 dis30">
+    <div class="distopwrap">
+    <div class="dislogo dislogo1">
+    </div>
+    <div class="disdis">
+    <strong>Bath&Body</strong> Bath and Body Products
+    </div>
+    </div>
+    <div class="disprice">
+      $129.95
+    </div>
+    <button class="disbut" onclick='displayInfoBathAndBody()' id="Bath&Body6"><i class="fa fa-shopping-cart faspace"></i> Display Product Information</button>
+  </div>
+  </div>
+ </div>
+</div>
+
+
+<footer class="footer-distributed">
+	<div class="footer-left">
+		<p class="footer-links">
+			<a class="link-1" href="HomePage.php">Home</a>
+
+			<a href="AboutUs.html">About Us</a>
+
+			<a href="ContactUs.html">Contact</a>
+			</p>
+			<p>Company Name &copy; 2015</p>
+	</div>
+</footer>
+
+    </body>
+</html>
